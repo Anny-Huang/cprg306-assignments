933dea9c593099ad41f6d5bffd64bd3ab1520630
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-05-20 12:44:17 -0600

    add week-2 and edit the root page of the project

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <>
+            <p>An-Ni Huang</p>
+            <Link href="https://github.com/Anny-Huang/cprg306-assignments.git">https://github.com/Anny-Huang/cprg306-assignments</Link>
+        </>
+    );
+}
