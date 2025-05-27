9257d2133da288039453101d584b65441a336548
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-05-27 10:25:09 -0600

    change clour scheme

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item(props) {
   return (
-    <li className="p-2 m-4 bg-slate-900 max-w-sm">
-      <h2 className="text-xl font-bold  text-white">{props.name}</h2>
-      <p className="text-sm text-white">Buy {props.quantity} in {props.category}</p>
+    <li className="p-2 m-4 bg-sky-100 max-w-sm">
+      <h2 className="text-xl font-bold  text-blue-800">{props.name}</h2>
+      <p className="text-sm text-blue-800">Buy {props.quantity} in {props.category}</p>
     </li>
   );
 }
