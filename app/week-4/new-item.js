65be43080636ad402ab79450d5cce15b983a4533
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-05-27 12:21:55 -0600

    1. initialize state variable 2. create incement and decrement functions

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -2,6 +2,20 @@
 import useState from 'react';
 
 export default function NewItem() {
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        let currentQuantity = quantity.valueof();
+        if (currentQuantity < 20 ) {
+            setQuantity(currentQuantity + 1);
+        }        
+    }
+
+    const decrement = () => {
+        let currentQuantity = quantity.valueof();
+        if (currentQuantity > 1) {
+            setQuantity(currentQuantity - 1);
+        }
+    }
  
-    
 }
