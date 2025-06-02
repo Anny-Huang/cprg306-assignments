d47da18572cbd28918edbae9f6711db28c2cc179
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-06-02 12:48:07 -0600

    Add quantity control buttons in new-item.js and render in page.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,21 +1,54 @@
 "use client";
-import useState from 'react';
+import { useState } from "react";
 
 export default function NewItem() {
-    const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(1);
 
-    const increment = () => {
-        let currentQuantity = quantity.valueof();
-        if (currentQuantity < 20 ) {
-            setQuantity(currentQuantity + 1);
-        }        
+  const increment = () => {
+    let currentQuantity = quantity.valueOf();
+    if (currentQuantity < 20) {
+      setQuantity(currentQuantity + 1);
     }
+  };
 
-    const decrement = () => {
-        let currentQuantity = quantity.valueof();
-        if (currentQuantity > 1) {
-            setQuantity(currentQuantity - 1);
-        }
+  const decrement = () => {
+    let currentQuantity = quantity.valueOf();
+    if (currentQuantity > 1) {
+      setQuantity(currentQuantity - 1);
     }
- 
+  };
+
+  let baseButtonStyle = "w-8 rounded text-white";
+  const isIncrementDisabled = quantity === 20;
+  const isDecrementDisabled = quantity === 1;
+
+  const GetButtonStyle = (isDisabled) => {
+    if (isDisabled) {
+      return `${baseButtonStyle} bg-gray-400`;
+    } else {
+      return `${baseButtonStyle} bg-blue-600 hover:bg-blue-800 cursor-pointer`;
+    }
+  };
+
+  return (
+    <div className="flex justify-between bg-white max-w-35 rounded p-2 ml-auto mr-auto mt-5">
+      <span className="text-black">{quantity}</span>
+      <div className="flex gap-2">
+        <button
+          disable={isDecrementDisabled}
+          className={GetButtonStyle(isDecrementDisabled)}
+          onClick={decrement}
+        >
+          -
+        </button>
+        <button
+          disable={isIncrementDisabled}
+          className={GetButtonStyle(isIncrementDisabled)}
+          onClick={increment}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,5 +1,10 @@
 import NewItem from './new-item';
 
 export default function Page() {
+    return (
+        <main>
+            <NewItem />
+        </main>
+    );
 
 }
